4b20acc98420fbded9d26fd38992d49baf040304
aditya-goyal3302 <agoyal2505.ca21@chitkara.edu.in>
2024-02-01 11:05:11 +0530

    system change

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
+const path = require('path')
 const MONGODB_URI =
   'mongodb+srv://agdoie-app:0ISZL1RJpYp6FDUM@cluster0.7hawrym.mongodb.net/sample';
 const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const port = 2984;
 const User = require('./models/users');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,"Public")));
 
 app.get('/adduser', (req, res, next) => {
-    res.render('add-user');
+    res.render('add-user',{
+        pageTitle:'Add User',
+        path:'/adduser',
+    });
 })
 
 app.post('/adduser', (req, res, next) => {
@@ -26,18 +31,50 @@ app.post('/adduser', (req, res, next) => {
         .save()
         .then(result => {
             console.log('Created User');
-            res.redirect('/');
+            res.redirect('/adduser');
         })
         .catch(err => {
             console.log(err);
         });
 })
 
-app.get('/',(req,res,next)=>{
-    res.render('index');
+
+app.get('/users',(req,res,next)=>{
+    currentPage = +req.query.page || 1;
+    perPage = 2;
+    let totalItems;
+    User.find()
+    .countDocuments()
+    .then(t=>{
+        totalItems = t;
+        return User.find()
+        .skip((currentPage-1)*perPage)
+        .limit(perPage)
+    })
+    
+    .then(users=>{
+        res.render('users',{
+            users:users,
+            pageTitle:'Users',
+            path:'/users',
+            totalItems:totalItems,
+            currentPage:currentPage,
+            hasNextPage:perPage*currentPage < totalItems,
+            hasPreviousPage:currentPage>1,
+            nextPage:currentPage+1,
+            previousPage:currentPage-1,
+            lastPage:Math.ceil(totalItems/perPage),
+        })
+    })
+    .catch(err=>{
+        console.log(err);
+    })
 })
 
 
+app.get('/',(req,res,next)=>{
+    res.render('index');
+})
 
 mongoose
     .connect(MONGODB_URI)
